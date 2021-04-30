66217253ceffb1dea11659219a7a05011ef9d642
yonglee <yonglee@student.42seoul.kr>
2021-04-30 14:12:38 +0900

    FIX: #48 Server Destructor & parseCGIResult
    서버 소멸자를 생성했습니다.
    parseCGIResult의 조건을 변경했습니다.

--- srcs/Parser.cpp
+++ srcs/Parser.cpp
@@ -73,7 +73,7 @@ void			Parser::getClientConf(Client &client, Request &req, std::vector<config> &
         client.conf = elmt;
         client.conf["path"] = req.uri.substr(0, req.uri.find("?"));
         if (elmt.find("root") != elmt.end())
-        	client.conf["path"].replace(0, tmp.size(), elmt["root"]); 
+        	client.conf["path"].replace(0, tmp.size(), elmt["root"]);
     }
     for (std::map<std::string, std::string>::iterator it(to_parse["server|"].begin()); it != to_parse["server|"].end(); ++it)
     {
@@ -393,8 +393,10 @@ void		Parser::parseCGIResult(Client &client)
         client.res.headers[key] = value;
         key.clear();
         value.clear();
-        if (headers[pos] == '\r' || headers[pos] == '\n')
-            pos++;
+        if (headers[pos] == '\r')
+			pos++;
+		if (headers[pos] == '\n')
+			pos++;
     }
     pos = client.res.body.find("\r\n\r\n") + 4;
     client.res.body = client.res.body.substr(pos);
